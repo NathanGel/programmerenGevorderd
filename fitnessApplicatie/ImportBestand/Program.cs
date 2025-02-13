b3e526cbc6716e93d8b64d7ea1091ecea49ff489
NathanGel <nathan.geleyn@student.hogent.be>
2025-02-13 21:22:35 +0100

    update

---
+++ fitnessApplicatie/ImportBestand/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace ImportBestand {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- fitnessApplicatie/fitnessApplicatie/SessieBeheer.cs
+++ fitnessApplicatie/fitnessApplicatie/SessieBeheer.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace fitnessApplicatie {
     class SessieBeheer {
         public SessieBeheer() {
-            //Dictionary<>;
-            //Dictionary<>;
+            Dictionary<int, List<RunningSession>> klantNr = new Dictionary<int, List<RunningSession>> { };
+            Dictionary<DateTime, List<RunningSession>> datum = new Dictionary<DateTime, List<RunningSession>> { };
         }
-        public void GeefSessiesKlant(int klantNr) {
-
+        public void GeefSessiesKlant(string klantNr) {
+            Console.Clear();
         }
         public void GeefSessiesDatum(DateTime datum) {
-
+            Console.Clear();
         }
     }
 }
